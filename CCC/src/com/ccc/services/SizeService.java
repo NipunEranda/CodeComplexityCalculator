919c5d2e685c011899d28a1cabaf1476d5c42a99
Prasad <Prasad@DESKTOP-3GG6746>
2020-05-10 10:48:29 +0530

    Size Clz updated

--- CCC/src/com/ccc/services/SizeService.java
+++ CCC/src/com/ccc/services/SizeService.java
@@ -7,18 +7,17 @@ import com.ccc.model.Line;
 
 public interface SizeService {
 
-	public boolean isAMethod(Line line);
 	public String getMethodName(String line);
+	public void getClassNames(CustomFile file);
+	public void getObjectSet(CustomFile file);
 	public void setEndLineNumber(CustomFile file);
 	public int getEndLineNumber(CustomFile file, Line line);
 	public void getMethodSet(CustomFile file);
-	public void getCalledMethodSet(CustomFile file);
-	public void getCalledMethodSet_DF(CustomFile ifile, ArrayList<CustomFile> fileList);
-	public void getMethodListFull(CustomFile file);
+	public void getCalledMethodSet_DF(CustomFile ifile);
 	public void getGlobalVariableSet(CustomFile file);
 	public void getGlobalVariableListInReg(CustomFile file);
 	public void getGlobalVariableListInRec(CustomFile file);
-	
+	public void getSystemMethods(CustomFile file);
 	
 	public void getRecursiveMethods(CustomFile file);
 	public void getRegularMethods(CustomFile file);
@@ -27,18 +26,22 @@ public interface SizeService {
 	public void getRecInRec(CustomFile file);
 	public void getRegInRec(CustomFile file);
 	
-	
-	public void getMethods_DF(ArrayList<CustomFile> fileList);
-	public void getRegInReg_DF(ArrayList<CustomFile> fileList);
-	public void getRecInReg_DF(ArrayList<CustomFile> fileList);
-	public void getRecInRec_DF(ArrayList<CustomFile> fileList);
-	public void getRegInRec_DF(ArrayList<CustomFile> fileList);
-	public void getGlobalVariableList_DF(CustomFile ifile, ArrayList<CustomFile> fileList);
-	public void getGlobalVariableListInReg_DF(ArrayList<CustomFile> fileList);
-	public void getGlobalVariableListInRec_DF(ArrayList<CustomFile> fileList);
+	public void getObjectSet_DF(CustomFile ifile);
+	public void getSystemMethods_DF(CustomFile ifile);
+	public void getMethods_DF();
+	public void getRegInReg_DF();
+	public void getRecInReg_DF();
+	public void getRecInRec_DF();
+	public void getRegInRec_DF();
+	public void getGlobalVariableList_DF(CustomFile ifile);
+	public void getGlobalVariableListInReg_DF();
+	public void getGlobalVariableListInRec_DF();
 	
 	public void process1(CustomFile file);
-	public void process2(ArrayList<CustomFile> fileList);
-	public void process3(ArrayList<CustomFile> fileList);
+	public void process2();
+	public void getKeywords(CustomFile file);
+	public void getOperators(CustomFile file);
+	
+	
 	
 }
